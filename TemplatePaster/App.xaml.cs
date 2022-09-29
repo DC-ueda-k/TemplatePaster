d2a7ec1799c13dbb18ec8b199dabb41a26f9b3d1
DC-ueda-k <keijirou.ueda.j3r@jpgr.denso.com>
2022-09-29 15:53:04 +0900

    issue #2

--- TemplatePaster/App.xaml.cs
+++ TemplatePaster/App.xaml.cs
@@ -13,5 +13,38 @@ namespace TemplatePaster
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var icon = GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream;
+
+            // タスクアイコンのコンテキストメニューにアイテムを追加
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            menu.Items.Add("終了", null, Exit_Click); // 左から：ツールチップ、？、発火するイベント
+
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Visible = true,
+                Icon = new System.Drawing.Icon(icon),
+                Text = "TemplatePaster"  // ツールチップに表示される文字列
+            };
+
+            notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(NotifyIcon_Click);
+        }
+
+        private void NotifyIcon_Click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                var wnd = new MainWindow();
+                wnd.Show();
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+        }
     }
 }
