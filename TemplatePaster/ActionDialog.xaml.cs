f1da056f224856eb38a605a64555bc57d163e877
DC-ueda-k <keijirou.ueda.j3r@jpgr.denso.com>
2023-03-03 18:34:56 +0900

    とりあえずダイアログにチェックボックス追加したりPasteObject変更したりはした

--- TemplatePaster/ActionDialog.xaml.cs
+++ TemplatePaster/ActionDialog.xaml.cs
@@ -3,43 +3,62 @@ using System.Windows;
 
 namespace TemplatePaster
 {
-  /// <summary>
-  /// Dialog.xaml の相互作用ロジック
-  /// </summary>
-  public partial class ActionDialog : Window
-  {
-    private Action<PasteObject> m_Action;
-
-    public ActionDialog(Action<PasteObject> addAction, PasteObject initValue)
+    /// <summary>
+    /// Dialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ActionDialog : Window
     {
-      InitializeComponent();
-      m_Action = addAction;
-      NameTextBox.Text = initValue.Name;
-      PasteStringTextBox.Text = initValue.PasteString;
-    }
+        /// <summary>
+        /// OKボタン押下時の追加処理
+        /// </summary>
+        private Action<PasteObject> m_Action;
 
-    private void OkButton_Click(object sender, RoutedEventArgs e)
-    {
-      var pasteObject = new PasteObject()
-      {
-        Name = NameTextBox.Text,
-        PasteString = PasteStringTextBox.Text,
-      };
-      m_Action(pasteObject);
-      Close();
-    }
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ActionDialog(Action<PasteObject> addAction, PasteObject initValue)
+        {
+            InitializeComponent();
+            m_Action = addAction;
+            NameTextBox.Text = initValue.Name;
+            PasteStringTextBox.Text = initValue.PasteString;
+            IsHiddenCheckBox.IsChecked = initValue.IsHidden;
+        }
 
-    private void CancelButton_Click(object sender, RoutedEventArgs e)
-    {
-      NameTextBox.Text = "";
-      PasteStringTextBox.Text = "";
-      Close();
-    }
+        /// <summary>
+        /// OKボタン押下時の処理
+        /// </summary>
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            var pasteObject = new PasteObject()
+            {
+                Name = NameTextBox.Text,
+                PasteString = PasteStringTextBox.Text,
+                IsHidden = IsHiddenCheckBox.IsChecked ?? false,
+            };
+            m_Action(pasteObject);
+            Close();
+        }
 
-    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-    {
-      e.Cancel = true;
-      Hide();
+        /// <summary>
+        /// キャンセルボタン押下時の処理
+        /// </summary>
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            NameTextBox.Text = "";
+            PasteStringTextBox.Text = "";
+            IsHiddenCheckBox.IsChecked = false;
+
+            Close();
+        }
+
+        /// <summary>
+        /// ウィンドウを閉じるときの処理
+        /// </summary>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
     }
-  }
 }
